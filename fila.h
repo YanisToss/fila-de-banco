9d4965607a8f1fabd86d801765ecfa165ce7dc61
Gilberto Charles <gilbertocharles1@gmail.com>
2016-05-24 18:17:21 -0300

    Add files via upload

---
+++ fila.h
@@ -0,0 +1,13 @@
+typedef struct Pessoa{
+
+  char nome[40];
+  int senha;
+}pessoa;
+
+typedef struct fila{
+  struct Pessoa;
+  int tempo;
+  int ini;
+  int fim;
+
+};
---
+++ main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "fila.h"
+
+
+int main(int argc, char const *argv[]) {
+
+  /*implementar a fila
+  mostrar o total de clientes atendidos
+  tempo médio
+  total de hora extra
+  geral de clientes atendidos
+  tempo médio geral na fila
+  maior e menor espera na fila
+  */
+
+
+
+
+  return 0;
+}
